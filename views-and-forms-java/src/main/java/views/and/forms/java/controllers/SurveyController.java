4ea558b72f5dd2e6399b69c91a914e7f8d4c2cb4
Holly Williams <williamsh@objectcomputing.com>
2020-04-01 11:58:26 -0500

    added exception mapping

--- views-and-forms-java/src/main/java/views/and/forms/java/controllers/SurveyController.java
+++ views-and-forms-java/src/main/java/views/and/forms/java/controllers/SurveyController.java
@@ -1,11 +1,13 @@
 package views.and.forms.java.controllers;
 
+import io.micronaut.http.HttpRequest;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Consumes;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Error;
 import io.micronaut.validation.validator.Validator;
 import io.micronaut.views.ModelAndView;
 import io.micronaut.views.View;
@@ -15,10 +17,14 @@ import views.and.forms.java.model.FormData;
 
 import javax.inject.Inject;
 import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Controller("/")
 public class SurveyController {
@@ -32,24 +38,22 @@ public class SurveyController {
     @View("home")
     public FormData home() {
 
-        if (LOG.isInfoEnabled()) LOG.info("Sending home page " );
+        if (LOG.isInfoEnabled()) {
+            LOG.info("Sending home page ");
+        }
         return new FormData();
 
     }
 
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Post("/survey")
-    public ModelAndView processHomeScreen(@Body FormData formData) {
+    public ModelAndView processHomeScreen(@Body @Valid FormData formData) {
 
         if (LOG.isInfoEnabled()) {
-            LOG.info(formData.getUserName() + ": has a chocolate preference of: " + formData.getChocolate());
+            LOG.info( "{} has a chocolate preference of: {}", formData.getUserName(), formData.getChocolate());
         }
 
         Set<ConstraintViolation<FormData>> constraintViolations = validator.validate(formData);
-
-        if (LOG.isInfoEnabled()) {
-            LOG.info("We have " + constraintViolations.size() + " constraintViolation(s) " );
-        }
 
         if (constraintViolations.size() > 0) {
             List<String> errors = new ArrayList<String>();
@@ -66,6 +70,38 @@ public class SurveyController {
         else {
             return new ModelAndView("thankyou", formData);
         }
+
+    }
+
+    @View("home")
+    @Error(exception = ConstraintViolationException.class)
+    public FormData constraintsErrors(HttpRequest request, ConstraintViolationException cv) {
+
+        if (LOG.isInfoEnabled()) {
+            LOG.info("In @Error meth ");
+        }
+
+        Set<ConstraintViolation<?>> constraintViolations = cv.getConstraintViolations();
+        AtomicReference<FormData> formDataToReturn = new AtomicReference<>(new FormData());
+        Optional<FormData> optionalFormData = request.getBody(FormData.class);
+
+        optionalFormData.ifPresent(formData -> {
+            List<String> errors = new ArrayList<String>();
+
+            for (ConstraintViolation<?> violation : constraintViolations) {
+                if (LOG.isInfoEnabled()) {
+                    LOG.info("Error: {}", violation.getMessage());
+                }
+                errors.add(violation.getMessage());
+
+            }
+            Collections.reverse(errors);
+            formData.setErrors(errors);
+            formDataToReturn.set(formData);
+
+        });
+
+        return formDataToReturn.get();
 
     }
 
--- views-and-forms-java/src/main/java/views/and/forms/java/model/FormData.java
+++ views-and-forms-java/src/main/java/views/and/forms/java/model/FormData.java
@@ -20,7 +20,6 @@ public class FormData {
     public FormData() {
     }
 
-
     public String getUserName() {
         return userName;
     }
